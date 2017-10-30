fb1c539c6478288b942ef34efd9a3353225e11fb
Bruno Melofiro <bruno@brunomelofiro.com>
2017-10-30 14:43:20 -0400

    add parallax hero img

--- wp-content/themes/FoundationPress/page-templates/page-home.php
+++ wp-content/themes/FoundationPress/page-templates/page-home.php
@@ -1,20 +1,28 @@
 <?php
 /*
-Template Name: Full Width
+Template Name: Homepage
 */
 get_header(); ?>
 
-<?php get_template_part( 'template-parts/featured-image' ); ?>
+<!--?php get_template_part( 'template-parts/featured-image' ); ?-->
+<script src="wp-content/themes/FoundationPress/node_modules/jquery-parallax.js/parallax.min.js"></script>
 
-<div class="main-wrap full-width" role="main">
+<div class="full-width" role="main">
 
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 	<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
+		<?php
+		$image = wp_get_attachment_url( get_post_thumbnail_id($post->ID));
+		if($image) :
+		 ?>
 		<header>
-
-			<h1 class="entry-title"><?php the_title(); ?></h1>
+		<div class="hero-img" data-parallax="scroll" data-image-src="<?php echo $image ?>">
+			<div style="width:100%; height:25rem;"></div>
+		</div>
 		</header>
+	<?php endif; ?>
+
 		<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
 		<div class="entry-content">
 			<?php the_content(); ?>
